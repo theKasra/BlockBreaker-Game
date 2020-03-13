11c947b5cc4ec2effc796e34776da52669d53f2e
theKasra <thekasra@outlook.com>
2020-03-13 06:22:04 +0330

    Win and Score UI

---
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+    // config params
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    // state
+    [SerializeField] int currentScore = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+}
