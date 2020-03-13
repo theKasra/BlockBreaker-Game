41fbc9532fb248c273e7b1b435c5717da29d4fce
theKasra <thekasra@outlook.com>
2020-03-13 06:25:27 +0330

    wait what...

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class Block : MonoBehaviour
 {
     // config params
     [SerializeField] AudioClip breakSound;
 
+    // cached references
+    Level level;
+    GameStatus gameStatus;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestroyed();
+        gameStatus.AddToScore();
     }
 }
---
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // config params
+    [SerializeField] int breakableBlocks;   // debugging purposes
+
+    // cached references
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if(breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
